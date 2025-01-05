f466841b8291b7db890ac25261dac88f8e244805
calin-precupetu <calinprecupetu@gmail.com>
2025-01-05 11:23:13 +0100

    Rename folders

---
+++ dapp/src/config/config.devnet.ts
@@ -0,0 +1,9 @@
+import { EnvironmentsEnum } from 'types';
+
+export * from './sharedConfig';
+
+export const contractAddress =
+  'erd1qqqqqqqqqqqqqpgqvrka92sreq6l5323jhfrwl3akph965cnd8ssd9j8e3';
+export const API_URL = 'https://devnet-template-api.multiversx.com';
+export const sampleAuthenticatedDomains = [API_URL];
+export const environment = EnvironmentsEnum.devnet;
